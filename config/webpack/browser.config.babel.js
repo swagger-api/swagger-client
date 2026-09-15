f0bd6a8ae42c5df1c881cccdf8cff40f4c06a074
Robert Hebel <robert.hebel@smartbear.com>
2026-09-15 08:40:03 +0200

    chore(config): update webpack bundle size thresholds

--- config/webpack/browser.config.babel.js
+++ config/webpack/browser.config.babel.js
@@ -63,8 +63,8 @@ const browserMin = {
   devtool: 'source-map',
   performance: {
     hints: 'error',
-    maxEntrypointSize: 590000, // Increased for OpenAPI 3.2 support
-    maxAssetSize: 60000000, // Increased for bundle stats file
+    maxEntrypointSize: 640000, // temp bump due to apidom minor version pinning
+    maxAssetSize: 70000000, // temp bump due to apidom minor version pinning
   },
   output: {
     path: path.resolve('./dist'),
